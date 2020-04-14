054307b4ee16f28d23fa9730977584c34ffb6673
Pradeep Kumar <pradeep.vwa@gmail.com>
2020-04-14 09:12:03 +0530

    AngularJS PHP Projects

--- angularcode_voting/ajax/updateVote.php
+++
@@ -1,8 +0,0 @@
-<?php
-include('../includes/config.php');
-$id = $_GET['id'];
-$votes = $_GET['votes'];
-$query="update posts set votes='$votes' where id='$id'";
-$result = $mysqli->query($query) or die($mysqli->error.__LINE__);
-// $affected = $mysqli->affected_rows;
-?>
