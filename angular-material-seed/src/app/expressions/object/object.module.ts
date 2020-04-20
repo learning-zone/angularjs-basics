da81ff4979c02bdea762e17cac4db9bf17547ad9
Pradeep Kumar <pradeep.vwa@gmail.com>
2020-04-20 12:40:50 +0530

    Object Expressions

--- angular-material-seed/src/app/expressions/object/object.module.ts
+++ angular-material-seed/src/app/expressions/object/object.module.ts
@@ -1,11 +1,11 @@
 import * as angular from 'angular';
 
 import { routes } from './object.routes';
-import ObjectComponent from './object.component';
+import objectComponent from './object.component';
 import { ObjectService } from './object.service';
 
 export default angular.module('string', [])
     .config(routes)
-    .component("object", ObjectComponent)
+    .component("object", objectComponent)
     .service('objectService', ObjectService)
     .name;
