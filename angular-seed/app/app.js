ce08ead2fd4b745d04a5428f78bca8b5f67c4fe7
Pradeep Kumar <pradeep.vwa@gmail.com>
2020-04-22 14:56:21 +0530

    Angular Seeed

--- angular-seed/app/app.js
+++ angular-seed/app/app.js
@@ -1,14 +1,18 @@
-'use strict';
+// Define the `phonecatApp` module
+var phonecatApp = angular.module('phonecatApp', []);
 
-// Declare app level module which depends on views, and core components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  $locationProvider.hashPrefix('!');
-
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+// Define the `PhoneListController` controller on the `phonecatApp` module
+phonecatApp.controller('PhoneListController', function PhoneListController($scope) {
+  $scope.phones = [
+    {
+      name: 'Nexus S',
+      snippet: 'Fast just got faster with Nexus S.'
+    }, {
+      name: 'Motorola XOOM™ with Wi-Fi',
+      snippet: 'The Next, Next Generation tablet.'
+    }, {
+      name: 'MOTOROLA XOOM™',
+      snippet: 'The Next, Next Generation tablet.'
+    }
+  ];
+});
