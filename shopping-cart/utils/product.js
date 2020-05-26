a638ec5d7807597e998c67cc308c17435aa6d24d
Pradeep Kumar <pradeep.vwa@gmail.com>
2020-05-26 19:03:11 +0530

    SQL Query Update

--- shopping-cart/utils/product.js
+++ shopping-cart/utils/product.js
@@ -22,17 +22,17 @@ class Product{
 	// Add Product
 	async addProducts(params){
 
-		let insert_statement = "INSERT INTO `product` (`product_name`, `product_description`, `sku`, `serial_number`, `purchase_price`, `selling_price`, `quantity`, `barcode`, `reordered_stock_amount`, `product_image`, `category_id`, `wharehouse_id`, `creation_time`) VALUES ( ? )";  
+		let insert_statement = "INSERT INTO `product` (`product_name`, `product_description`, `sku`, `serial_number`, `purchase_price`, `selling_price`, `quantity`, `barcode`, `reordered_stock_amount`, `product_image`, `category_id`, `wharehouse_id`) VALUES ( ? )";  
   		let values = [  
-			params['product_name'],params['product_description'],params['sku'],params['serial_number'],params['purchase_price'],
-			params['selling_price'],params['quantity'],params['barcode'],params['reordered_stock_amount'],params['product_image'],
-			params['category'],params['wharehouse'],params[Date.now()]
+			params['product_name'], params['product_description'], params['sku'], params['serial_number'], params['purchase_price'],
+			params['selling_price'], params['quantity'], params['barcode'], params['reordered_stock_amount'], params['product_image'],
+			params['category'], params['warehouse']
 		];  
 			
 		console.log("insert_statement: "+insert_statement);
 		console.log("values: "+values);
 		try {
-			//return await this.db.query(insert_statement, [values]);
+			return await this.db.query(insert_statement, [values]);
 		} catch (error) {
 			console.error(error);
 			return null;
